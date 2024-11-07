f7f4f39552030e762b91eb0d12da540cfb9f8920
alinaveed484 <alinaveed484@gmail.com>
2024-11-07 16:13:37 +0500

    Finally Init2

---
+++ src/main/java/com/angoor/taleemhub/TaleemHubApplication.java
@@ -0,0 +1,13 @@
+package com.angoor.taleemhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaleemHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaleemHubApplication.class, args);
+    }
+
+}
