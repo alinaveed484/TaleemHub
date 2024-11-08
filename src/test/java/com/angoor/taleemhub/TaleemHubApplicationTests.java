12f34140f9f6332b03977c89c870dfd25709767f
alinaveed484 <alinaveed484@gmail.com>
2024-11-08 16:41:12 +0500

    feat: Added the code for a Single Test

--- src/test/java/com/angoor/taleemhub/TaleemHubApplicationTests.java
+++ src/test/java/com/angoor/taleemhub/TaleemHubApplicationTests.java
@@ -1,13 +1,27 @@
 package com.angoor.taleemhub;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TaleemHubApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;  // Inject MockMvc for HTTP testing
+
     @Test
-    void contextLoads() {
+    @WithMockUser(username = "admin", password = "admin")
+    public void testSayHelloEndpoint() throws Exception {
+        mockMvc.perform(get("/hello"))  // Perform a GET request to /hello
+                .andExpect(MockMvcResultMatchers.status().isOk())  // Expect HTTP 200 OK
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Hello, World!"));  // Check "status" valu
     }
 
 }
