f51b438f75c64aa4ba8ea82ce085c3fac3144c91
Hamza Omer <Hamza.omer.zaki@gmail.com>
2024-11-10 02:03:39 +0500

    Added ChatController and implemented some functionality.
    Currently, the ChatController class creates a unique chat-key for the student and teacher and sends the messages to that dynamically created endpoint. I added attributesof chat and message classes but havent added them to database. Also lots of khadshaat about identification and verification of users, will discuss in person. Set up the basic flow of the messaging feature atleast.

--- src/main/java/com/angoor/project/model/Chat.java
+++ src/main/java/com/angoor/project/model/Chat.java
@@ -1,8 +1,36 @@
 package com.angoor.project.model;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.*;
 
+
 public class Chat {
 
+    private Integer teacherID;
+    private Integer studentID;
+
+    private List<Message> messages = new ArrayList<>();
+
+    public Chat(Integer teacherID, Integer studentID) {
+        this.teacherID = teacherID;
+        this.studentID = studentID;
+    }
+
+    public Integer getTeacherID() {
+        return teacherID;
+    }
+
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void addMessage(Message message) {
+        messages.add(message);
+    }
 }
